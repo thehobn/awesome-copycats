f80e09cb11e652a2494c39ba0c79864a24483825
thehobn <thehobn@gmail.com>
2014-01-11 14:29:20 -0800

    Update new.rc.lua

--- new.rc.lua
+++ new.rc.lua
@@ -45,7 +45,6 @@ function run_once(cmd)
   awful.util.spawn_with_shell("pgrep -u $USER -x " .. findme .. " > /dev/null || (" .. cmd .. ")")
 end
 
-run_once("xterm")
 run_once("unclutter")
 -- }}}
 
